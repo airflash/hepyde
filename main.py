4b03d346dbe87318d681091bbc8b0ad71a1fba36
Andrew Scherban <scherbana@digicode.net>
2017-07-05 15:55:56 +0300

    ewr

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def simple_app(environ, start_response):
+def application(environ, start_response):
     status = '200 OK'
-    response_headers = [('Content-type','text/plain')]
+    output = 'Hello World!\n'
+    response_headers = [('Content-type', 'text/plain'),
+                        ('Content-Length', str(len(output)))]
     start_response(status, response_headers)
-    return ['Hello world!\n']
-
-#print("helly dynos")
+    return [output]
