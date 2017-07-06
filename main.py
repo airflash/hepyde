0f520143ff16953f71277596cdd457e0ad5ec42b
Andrew Scherban <scherbana@digicode.net>
2017-07-06 11:58:20 +0300

    add Postgres connect

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import json
+import os
+import psycopg2
+import urlparse
+
+def connectToDB():
+	urlparse.uses_netloc.append("postgres")
+	url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+	conn = psycopg2.connect(
+    	database=url.path[1:],
+    	user=url.username,
+    	password=url.password,
+    	host=url.hostname,
+    	port=url.port
+	)
 
 def application(environ, start_response):
     status = '200 OK'
     output = 'Hello World!\n\n'
     
-    for keys,values in environ.items():
-    	output += str(keys) + '\n'
-    	output += str(values) + '\n'
+    # for keys,values in environ.items():
+    # 	output += str(keys) + '\n'
+    # 	output += str(values) + '\n'
+
+	connectToDB()
 
     #output += json.dumps(environ)
 
@@ -17,3 +34,4 @@ def application(environ, start_response):
     
     # можно использовать [data.encode('utf-8')]. 
     # также The specs says that you can return an itterator here. So either ["OK"] or yield "OK" :)
+
