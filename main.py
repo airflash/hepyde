086318abcea2224b99f7d43c2bd71f2b63de11b5
Andrew Scherban <scherbana@digicode.net>
2017-07-06 12:51:18 +0300

    fsd

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def connectToDB():
 	urlparse.uses_netloc.append("postgres")
 	url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
-	# conn = psycopg2.connect(
- #    	database=url.path[1:],
- #    	user=url.username,
- #    	password=url.password,
- #    	host=url.hostname,
- #    	port=url.port
-	# )
+	conn = psycopg2.connect(
+    	database=url.path[1:],
+    	user=url.username,
+    	password=url.password,
+    	host=url.hostname,
+    	port=url.port
+	)
 
 def application(environ, start_response):
     status = '200 OK'
