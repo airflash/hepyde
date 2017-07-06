d29da911da05bde53f35395cc3e29b41664db997
Andrew Scherban <scherbana@digicode.net>
2017-07-06 11:29:42 +0300

    sdf

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import json
+
 def application(environ, start_response):
     status = '200 OK'
     output = 'Hello World!\n'
+    output += environ
     response_headers = [('Content-type', 'text/plain'),
                         ('Content-Length', str(len(output)))]
     start_response(status, response_headers)
